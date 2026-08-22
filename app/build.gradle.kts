223e2e2e4ded49c7fd80d7cf6576bf47771aa9c8
Dipankaj Singh <dipankajsingh2@gmail.com>
2026-08-22 17:35:46 +0530

    chore: bump version to 2.10.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dipdev.aiautocaptioner"
         minSdk = 24
         targetSdk = 37
-        versionCode = 21
-        versionName = "2.10.1"
+        versionCode = 22
+        versionName = "2.10.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
